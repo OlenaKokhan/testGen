07a06173ad6386073b79d79789f56e27c6ae503b
DEVELOPER\Helenka <olena.kokhan97@gmail.com>
2017-11-22 14:31:17 +0200

    Added migrations for user table

---
+++ migrations/m171122_121750_create_user_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `user`.
+ */
+class m171122_121750_create_user_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $tableOptions = null;
+
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'first_name' => $this->string()->notNull()->unique(),
+            'second_name' => $this->string()->notNull()->unique(),
+            'age' => $this->string(),
+            'gender' => $this->string(1)->defaultValue(1),
+            'photo' => $this->text(),
+            'photo_thumb' => $this->text(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+}
